4f288565c98ed5d7049f4ae0a0060a26641a2296
nayan457-bit <nayan.krishna.s68@kalvium.community>
2025-04-02 10:21:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import ItemList from "./components/ItemList";
+import React from 'react';
+import ItemList from './components/ItemList';
 
-// use the following link to get the data
-// `/doors` will give you all the doors.
-const API_URI = `https://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
-  // Get the existing item from the server
-  // const [items, setItems] = useState(null);
-  // pass the item to UpdateItem as a prop
+  const API_URI = 'https://jsonplaceholder.typicode.com/posts';  // Mock API
 
-  return <ItemList />;
+  return (
+    <div className="App">
+      <h1>My Item Management App</h1>
+      {/* Pass the API URI to ItemList */}
+      <ItemList apiUri={API_URI} />
+    </div>
+  );
 }
 
 export default App;
